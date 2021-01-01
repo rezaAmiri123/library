2acacdea6d90d9cd9fc742c608c79a02ccbd5f68
reza amiri <reza.amiri.ac@gmail.com>
2021-01-01 15:33:54 +0330

    add user model

---
+++ accounts/models.go
@@ -0,0 +1,14 @@
+package accounts
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	Username      string  `gorm:"column:username;unique_index"`
+	Password      string  `gorm:"column:password;not null"`
+	Email         string  `gorm:"column:email,unique_index"`
+	Bio           string  `gorm:"column:bio;size:1024"`
+	Image         *string `gorm:"column:image"`
+	EmailVerified bool    `gorm:"column:email_verified"`
+	IsActive      bool    `gorm:"column:is_active"`
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rezaAmiri123/library
 
 go 1.14
+
+require github.com/jinzhu/gorm v1.9.16 // indirect
