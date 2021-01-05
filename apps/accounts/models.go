f8ebca7088599e95b89d6c232b0a6956103941d9
reza amiri <reza.amiri.ac@gmail.com>
2021-01-05 14:02:41 +0330

    add follow model

--- apps/accounts/models.go
+++ apps/accounts/models.go
@@ -9,7 +9,9 @@ import (
 
 func AutoMigrate(db *gorm.DB) {
 	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Follow{})
 }
+
 type User struct {
 	gorm.Model
 	Username      string  `gorm:"column:username;unique_index"`
@@ -40,4 +42,56 @@ func (u *User) CheckPassword(password string) error {
 func (u *User) Update(data interface{}) error {
 	db := conf.GetDB()
 	return db.Model(u).Update(data).Error
+}
+
+type Follow struct {
+	gorm.Model
+	Follower   User
+	FollowerID uint
+	Followed   User
+	FollowedID uint
+}
+
+func (u User) Follow(ou User) error {
+	db := conf.GetDB()
+	var f Follow
+	err := db.FirstOrCreate(&f, Follow{
+		FollowerID: u.ID,
+		FollowedID: ou.ID,
+	}).Error
+	return err
+}
+
+func (u User) IsFollowed(ou User) bool {
+	db := conf.GetDB()
+	var f Follow
+	db.Where(Follow{
+		FollowerID: u.ID,
+		FollowedID: ou.ID,
+	}).First(&f)
+	return f.ID != 0
+}
+
+func (u User) UnFollow(ou User) error {
+	db := conf.GetDB()
+	err := db.Where(Follow{
+		FollowerID: u.ID,
+		FollowedID: ou.ID,
+	}).Delete(Follow{}).Error
+	return err
+}
+
+func (u User) GetFollowings() []User {
+	db := conf.GetDB()
+	var fs []Follow
+	var fus []User
+	tx := db.Begin()
+	tx.Where(Follow{FollowerID: u.ID}).Find(&fs)
+	for _, f := range fs {
+		var fu User
+		tx.Model(&f).Related(&fu, "Followed")
+		fus = append(fus, fu)
+	}
+	tx.Commit()
+	return fus
 }
